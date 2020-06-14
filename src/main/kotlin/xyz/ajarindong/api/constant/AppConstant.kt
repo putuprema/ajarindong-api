f8627de4a635b02c21379ccea567f7c5a8cee56a
putuprema <iputupremaananda@gmail.com>
2020-06-14 12:52:39 +0700

    change storage base path

--- src/main/kotlin/xyz/ajarindong/api/constant/AppConstant.kt
+++ src/main/kotlin/xyz/ajarindong/api/constant/AppConstant.kt
@@ -9,7 +9,7 @@ object AppConstant {
 
     const val DEFAULT_DATE_FORMAT = "dd/MM/yyyy"
 
-    const val STORAGE_BASE_PATH = "D:/AjarinDong_storage/data"
+    const val STORAGE_BASE_PATH = "/data"
     const val STORAGE_STUDENT_FOLDER = "/student"
     const val STORAGE_MENTOR_FOLDER = "/mentor"
     const val STORAGE_COURSE_CATEGORY_FOLDER = "/course-category"
